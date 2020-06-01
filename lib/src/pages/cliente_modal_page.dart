54e487308ddfb4eaa8df704489f2706406c4f871
claudio910103 <claudiovelazquezvazquez@gmail.com>
2020-06-01 18:03:00 -0500

    Se agrego validacion al modal del cliente

--- lib/src/pages/cliente_modal_page.dart
+++ lib/src/pages/cliente_modal_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tonyredapp/src/bloc/provider.dart';
+import 'package:tonyredapp/src/models/cliente_model.dart';
  
 class ClienteModalPage extends StatefulWidget {
   ClienteModalPage({Key key}) : super(key: key);
@@ -9,11 +11,20 @@ class ClienteModalPage extends StatefulWidget {
 
 class _ClienteModalState extends State<ClienteModalPage> {
   String dropdownValue = '1 Mes';
+  final formKey = GlobalKey<FormState>(); // Para validar formularios
+  final scaffoldKey = GlobalKey<ScaffoldState>(); // Para mostrar el snackbar
 
+  ClientesBloc clientesBloc;
+  ClienteModel cliente = new ClienteModel();
+  bool _guardando = false;
 
   @override
   Widget build(BuildContext context) {
+    clientesBloc = Provider.clientesBloc(context);
+
+
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: Text('Registrar Cliente'),
         actions: <Widget>[
@@ -33,24 +44,27 @@ class _ClienteModalState extends State<ClienteModalPage> {
 
   Widget _formularioCliente() {
     return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          SizedBox(height: 10.0),
-          _tituloSeccion('Datos Cliente:'),
-          _inputNombreCliente(),
-          _inputTelCliente(),
-          _inputModAntena(),
-          _inputDireccionIP(),
-          _tituloSeccion('Coordenadas:'),
-          _seccionUbicacion(),
-          _tituloSeccion('Pagos:'),
-          _selectMensualidad(),
-          _tituloSeccion('Fotografia Fachada:'),
-          _botonFotografia(),
-          SizedBox(height: 80.0),
-          _botonGuardarCliente() ,
-          SizedBox(height: 80.0)
-        ],
+      child: Form(
+              key: formKey,
+              child: Column(
+              children: <Widget>[
+                SizedBox(height: 10.0),
+                _tituloSeccion('Datos Cliente:'),
+                _inputNombreCliente(),
+                _inputTelCliente(),
+                _inputModAntena(),
+                _inputDireccionIP(),
+                _tituloSeccion('Coordenadas:'),
+                _seccionUbicacion(),
+                _tituloSeccion('Mensualidad:'),
+                _selectMensualidad(),
+                _tituloSeccion('Fotografia Fachada:'),
+                _botonFotografia(),
+                SizedBox(height: 80.0),
+                _botonGuardarCliente() ,
+                SizedBox(height: 80.0)
+              ],
+            ),
       ),
     );
 
@@ -72,7 +86,9 @@ class _ClienteModalState extends State<ClienteModalPage> {
     return Container(
       margin: EdgeInsets.only(bottom: 8.0),
       padding: EdgeInsets.symmetric(horizontal: 12.0),
-      child: TextField(
+      child: TextFormField(
+        initialValue: cliente.nombre,
+        textCapitalization: TextCapitalization.sentences,
         keyboardType: TextInputType.text,
         decoration: InputDecoration(
           prefixIcon: Icon( Icons.person_outline, color: Colors.black38),
@@ -81,6 +97,14 @@ class _ClienteModalState extends State<ClienteModalPage> {
           labelText: 'Nombre del cliente',
           hintText: 'Escriba el nombre del cliente...',
         ),
+        onSaved: (value) => cliente.nombre = value,
+        validator: (value) {
+          if ( value.length < 3 ) {
+            return 'Ingrese el nombre completo';
+          } else {
+            return null;
+          }
+        },
       )
     );
   }
@@ -89,7 +113,8 @@ class _ClienteModalState extends State<ClienteModalPage> {
     return Container(
       margin: EdgeInsets.only(bottom: 8.0),
       padding: EdgeInsets.symmetric(horizontal: 12.0),
-      child: TextField(
+      child: TextFormField(
+        initialValue: cliente.telefono,
         keyboardType: TextInputType.phone,
         decoration: InputDecoration(
           prefixIcon: Icon( Icons.phone_android, color: Colors.black38),
@@ -98,6 +123,14 @@ class _ClienteModalState extends State<ClienteModalPage> {
           labelText: 'Teléfono del cliente',
           hintText: 'Escriba el teléfono de contacto del cliente...',
         ),
+        onSaved: (value) => cliente.telefono = value,
+        validator: (value) {
+          if ( value.length < 8 ) {
+            return 'Ingrese el teléfono';
+          } else {
+            return null;
+          }
+        },
       )
     );
   }
@@ -106,7 +139,8 @@ class _ClienteModalState extends State<ClienteModalPage> {
     return Container(
       margin: EdgeInsets.only(bottom: 8.0),
       padding: EdgeInsets.symmetric(horizontal: 12.0),
-      child: TextField(
+      child: TextFormField(
+        initialValue: cliente.modeloAntena,
         keyboardType: TextInputType.text,
         decoration: InputDecoration(
           prefixIcon: Icon( Icons.wifi_tethering, color: Colors.black38),
@@ -115,6 +149,14 @@ class _ClienteModalState extends State<ClienteModalPage> {
           labelText: 'Modelo de la antena',
           hintText: 'Escriba el modelo de la antena...',
         ),
+        onSaved: (value) => cliente.modeloAntena = value,
+        validator: (value) {
+          if ( value.length < 3 ) {
+            return 'Ingrese el modelo de la antena';
+          } else {
+            return null;
+          }
+        },
       )
     );
   }
@@ -123,7 +165,8 @@ class _ClienteModalState extends State<ClienteModalPage> {
     return Container(
       margin: EdgeInsets.only(bottom: 8.0),
       padding: EdgeInsets.symmetric(horizontal: 12.0),
-      child: TextField(
+      child: TextFormField(
+        initialValue: cliente.direccionIP,
         keyboardType: TextInputType.text,
         decoration: InputDecoration(
           prefixIcon: Icon( Icons.settings_remote, color: Colors.black38),
@@ -132,6 +175,14 @@ class _ClienteModalState extends State<ClienteModalPage> {
           labelText: 'Direccion IP',
           hintText: 'Escriba la dirección IP de la antena...',
         ),
+        onSaved: (value) => cliente.direccionIP = value,
+        validator: (value) {
+          if ( value.length < 4 ) {
+            return 'Ingrese la dirección IP';
+          } else {
+            return null;
+          }
+        },
       )
     );
   }
@@ -157,7 +208,8 @@ class _ClienteModalState extends State<ClienteModalPage> {
       margin: EdgeInsets.only(bottom: 8.0),
       padding: EdgeInsets.only(left: 12.0),
       width: size.width * 0.47,
-      child: TextField(
+      child: TextFormField(
+        initialValue: cliente.latitud,
         keyboardType: TextInputType.text,
         decoration: InputDecoration(
           prefixIcon: Icon( Icons.place, color: Colors.black38),
@@ -166,6 +218,14 @@ class _ClienteModalState extends State<ClienteModalPage> {
           labelText: 'Latitud',
           hintText: 'Escriba la latitud...',
         ),
+        onSaved: (value) => cliente.latitud = value,
+        validator: (value) {
+          if ( value.length < 3 ) {
+            return 'Ingrese la latitud';
+          } else {
+            return null;
+          }
+        },
       )
     );
   }
@@ -176,7 +236,8 @@ class _ClienteModalState extends State<ClienteModalPage> {
       margin: EdgeInsets.only(bottom: 8.0),
       padding: EdgeInsets.only(right: 12.0),
       width: size.width * 0.47,
-      child: TextField(
+      child: TextFormField(
+        initialValue: cliente.longitud,
         keyboardType: TextInputType.text,
         decoration: InputDecoration(
           prefixIcon: Icon( Icons.place, color: Colors.black38),
@@ -185,6 +246,14 @@ class _ClienteModalState extends State<ClienteModalPage> {
           labelText: 'Longitud',
           hintText: 'Escriba la longitud...',
         ),
+        onSaved: (value) => cliente.longitud = value,
+        validator: (value) {
+          if ( value.length < 3 ) {
+            return 'Ingrese la longitud';
+          } else {
+            return null;
+          }
+        },
       )
     );
   }
@@ -198,45 +267,46 @@ class _ClienteModalState extends State<ClienteModalPage> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
 
-            Container(
-              padding: EdgeInsets.only(left: 12.0),
-              width: size.width * 0.47,
-              height: 65,
-              child: DropdownButton<String>(
-                value: dropdownValue,
-                icon: Icon(Icons.arrow_drop_down),
-                iconSize: 24,
-                elevation: 16,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                ),
-                underline: Container(
-                  height: 1,
-                  color: Colors.black38,
-                ),
-                isExpanded: true,
-                onChanged: (String newValue) {
-                  setState(() {
-                    dropdownValue = newValue;
-                  });
-                },
-                items: <String>['1 Mes', '2 Meses', '3 Meses', '4 Meses']
-                  .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  })
-                  .toList()
-              ),
-            ),
+            // Container(
+            //   padding: EdgeInsets.only(left: 12.0),
+            //   width: size.width * 0.47,
+            //   height: 65,
+            //   child: DropdownButton<String>(
+            //     value: dropdownValue,
+            //     icon: Icon(Icons.arrow_drop_down),
+            //     iconSize: 24,
+            //     elevation: 16,
+            //     style: TextStyle(
+            //       color: Colors.black,
+            //       fontSize: 16,
+            //     ),
+            //     underline: Container(
+            //       height: 1,
+            //       color: Colors.black38,
+            //     ),
+            //     isExpanded: true,
+            //     onChanged: (String newValue) {
+            //       setState(() {
+            //         dropdownValue = newValue;
+            //       });
+            //     },
+            //     items: <String>['1 Mes', '2 Meses', '3 Meses', '4 Meses']
+            //       .map<DropdownMenuItem<String>>((String value) {
+            //         return DropdownMenuItem<String>(
+            //           value: value,
+            //           child: Text(value),
+            //         );
+            //       })
+            //       .toList()
+            //   ),
+            // ),
 
             Container(
               margin: EdgeInsets.only(bottom: 10.0),
               padding: EdgeInsets.only(right: 12.0),
-              width: size.width * 0.47,
-              child: TextField(
+              width: size.width,
+              child: TextFormField(
+                initialValue: cliente.costoMensualidad.toString(),
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                   prefixIcon: Icon( Icons.monetization_on, color: Colors.black38),
@@ -245,6 +315,14 @@ class _ClienteModalState extends State<ClienteModalPage> {
                   labelText: 'Monto mensualidad',
                   hintText: '\$ 0.00',
                 ),
+                onSaved: (value) => cliente.costoMensualidad = int.parse(value),
+                validator: (value) {
+                  if ( int.parse(value) <= 0 ) {
+                    return 'Ingrese el costo de la mensualidad';
+                  } else {
+                    return null;
+                  }
+                },
               )
             )
 
@@ -287,8 +365,29 @@ class _ClienteModalState extends State<ClienteModalPage> {
       elevation: 1.0,
       color: Color.fromRGBO(30, 100, 247, 1.0),
       textColor: Colors.white,
-      onPressed: () {}
+      onPressed: (_guardando) ? null : _submit
     );
+  }
+
+  void _submit() async {
+    if ( !formKey.currentState.validate() ) return;
+    formKey.currentState.save();
+    
+    setState(() { _guardando = true; });
+
+    mostrarSnackbar('Cliente Guardado');
+    Navigator.pop(context); 
+  }
+
+  void mostrarSnackbar(String mensaje) {
+
+    final snackbar = SnackBar(
+      content: Text( mensaje ),
+      duration: Duration( milliseconds: 1500),
+    );
+
+    scaffoldKey.currentState.showSnackBar(snackbar);
+
   }
 
 }
